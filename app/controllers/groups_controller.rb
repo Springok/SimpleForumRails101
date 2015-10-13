4919f53bd298db0e4fc7d5c7f1ffeb88fbdc9ed5
Springok <wenchuan.lin.tw@gmail.com>
2015-10-13 15:06:46 +0800

    Add notice_message helper

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,10 @@
 class GroupsController < ApplicationController
 
 def index
+  # flash[:notice] = "午安～您好！"
+  # flash[:warning] = "test"
+  # flash[:info]= "info"
+  # flash[:danger]= "danger" 測試一下notice_message helper
 
 end
 
