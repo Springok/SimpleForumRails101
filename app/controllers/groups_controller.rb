9707d971ea613c438b91fc4adf6ace9396d5505a
Springok <wenchuan.lin.tw@gmail.com>
2015-10-13 16:01:11 +0800

    Add CURD for group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,11 +1,59 @@
 class GroupsController < ApplicationController
 
-def index
-  # flash[:notice] = "午安～您好！"
-  # flash[:warning] = "test"
-  # flash[:info]= "info"
-  # flash[:danger]= "danger" 測試一下notice_message helper
+  def index
+    # flash[:notice] = "午安～您好！"
+    # flash[:warning] = "test"
+    # flash[:info]= "info"
+    # flash[:danger]= "danger" 測試一下notice_message helper
+    @groups = Group.all
 
-end
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def show
+    @group = Group.find(params[:id])
+
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_path, notice: "新增討論板成功"
+    else
+      render :new
+    end
+
+
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "更新討論板成功"
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_path, alert: "討論板已刪除"
+  end
+
+  private
+  def group_params
+    params.require(:group).permit( :title, :description)
+  end
 
 end
