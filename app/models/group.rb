87b65c68fcdb8f11234e7fad50704c988d7f1b77
Springok <wenchuan.lin.tw@gmail.com>
2015-10-13 19:55:43 +0800

    Add posts CURD

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,4 @@
 class Group < ActiveRecord::Base
   validates :title, presence: true
-  has_many :posts
+  has_many :posts, dependent: :destroy
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ActiveRecord::Base
   belongs_to :group
+  validates :context, presence: true
 end
