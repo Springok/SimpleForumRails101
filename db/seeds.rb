4238c44d44f8833bab9b49b79836fc1f157a5705
Springok <wenchuan.lin.tw@gmail.com>
2015-10-15 12:30:18 +0800

    Add db:seed for faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "生成一測試帳號"
+puts "生成20個grou,每個group有30篇post"
+create_account = User.create(email: "example@gmail.com",
+                               password: "12345678",
+                               password_confirmation: "12345678",
+                               name: "麗子小姐"
+                              )
+
+20.times do |i|
+    Group.create(title: "Group_" + Faker::Lorem.word, description: Faker::Lorem.sentences, user_id: "1")
+    30.times do
+      Post.create(group_id: "#{i+1}", context: Faker::Lorem.sentences, user_id: "1")
+    end
+end
+
+
+
+
